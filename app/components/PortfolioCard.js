df30fd5ddf00da1b96aab254d6f7bd948d27688e
Matthew Jesuele <mjesuele@gmail.com>
2016-05-24 12:28:42 -0700

    Made some fixes so the component renders now :)

--- app/components/PortfolioCard.js
+++ app/components/PortfolioCard.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { PropTypes } from 'react';
 import './PortfolioCard.scss';
 
-const PortfolioCard = (imageUrl, text, linkUrl) => (
+const PortfolioCard = ({ imageUrl, text, linkUrl }) => (
   <div className="portfolio-card">
     <h2 className="portfolio-card-text">{text}{linkUrl}</h2>
     <img className="portfolio-card-img" src={imageUrl} role="presentation" />
   </div>
 );
 
+PortfolioCard.propTypes = {
+  linkUrl: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
-ReactDOM.render(
-  <PortfolioCard />,
-);
 export default PortfolioCard;
